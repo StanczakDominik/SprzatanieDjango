3a9ec9be6eabfedc9db1269c49a726a10da637af
Dominik Stańczak-Marikin <stanczakdominik@gmail.com>
2024-10-19 18:44:43 +0200

    feat: add admindocs

--- sprzatanie/urls.py
+++ sprzatanie/urls.py
@@ -20,6 +20,7 @@ from django.urls import path, include
 from django.conf import settings
 
 urlpatterns = [
+    path("admin/doc/", include("django.contrib.admindocs.urls")),
     path("admin/", admin.site.urls),
     path("dashboard/", include("dashboard.urls")),
     path("accounts/", include("django.contrib.auth.urls")),
